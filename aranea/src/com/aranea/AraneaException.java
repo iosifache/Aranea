e852a7eb6467e68884724ea4da74fc1c5d3079b0
George-Andrei Iosif <georgeiosif@live.com>
2020-11-30 18:50:12 +0200

    Implementează clasa AraneaException
    Issue: #3
    Implementează clasa părinte a tuturor excepțiilor din aplicație. În
    același timp, adaugă comentarii exemplelor de utilizare, excepții
    necesare și o valoare implicită în clasa AraneaLogger.
    
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

---
+++ aranea/src/com/aranea/AraneaException.java
@@ -0,0 +1,46 @@
+package com.aranea;
+
+import com.aranea.AraneaLogger.AraneaLoggerLevels;
+import java.lang.Exception;
+
+public class AraneaException extends Exception {
+
+  private AraneaLoggerLevels level;
+
+  public AraneaException(AraneaLoggerLevels level, String message) {
+    super(message);
+    this.level = level;
+  }
+
+  public void logException(AraneaLogger logger) {
+    logger.log(this.level, this.getMessage());
+  }
+
+  private void exemplifyUsage() {
+
+    try {
+
+      // Get logger instance and set output file
+      AraneaLogger logger = AraneaLogger.getInstance();
+      logger.setOutputFile("log.txt");
+
+      // Throw a standard exception and log it
+      try {
+        throw new AraneaException(AraneaLoggerLevels.ERROR, "Error example");
+      } catch (AraneaException e) {
+        e.logException(logger);
+      }
+    } catch (AraneaException e) {
+
+      // If an exception is thrown, exit the program
+      System.exit(1);
+    }
+  }
+
+  public class ChildException extends AraneaException{
+    public ChildException(){
+      super(AraneaLoggerLevels.ERROR, "An error occurred.");
+    }
+  }
+
+}
