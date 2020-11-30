3067d17c5ec1df3f2d969407c2dee122dd5fe93f
George-Andrei Iosif <georgeiosif@live.com>
2020-11-30 18:15:39 +0200

    Implementează clasa AraneaLogger
    Issue: #2
    Implementează clasa specializată pe jurnalizare, AraneaLogger.
    
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

---
+++ aranea/src/com/aranea/AraneaLogger.java
@@ -0,0 +1,98 @@
+package com.aranea;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.FileHandler;
+import java.util.logging.Filter;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.io.IOException;
+
+public class AraneaLogger {
+
+  private static AraneaLogger araneaLogger = null;
+  private static FileHandler outputFile = null;
+  private static Logger logger = null;
+  private static Level minLevel = null;
+
+  private AraneaLogger() {}
+
+  public void setOutputFile(String filename) {
+    logger = Logger.getLogger("com.aranea.AraneaLogger");
+    logger.setUseParentHandlers(false);
+
+    try {
+      outputFile = new FileHandler(filename, true);
+    } catch (IOException e) {
+      // TODO(@iosifache): Add custom exception after its base class implementation
+    }
+    outputFile.setFormatter(new AraneaLoggerFormatter());
+    outputFile.setFilter(new AraneaLoggerFilter());
+
+    logger.addHandler(outputFile);
+  }
+
+  public void setMinLevel(AraneaLoggerLevels level) {
+    minLevel = level.standardLevel;
+  }
+
+  public static AraneaLogger getInstance() {
+    if (araneaLogger == null) araneaLogger = new AraneaLogger();
+    return araneaLogger;
+  }
+
+  public void log(AraneaLoggerLevels level, String message) {
+    logger.log(level.standardLevel, message);
+  }
+
+  public enum AraneaLoggerLevels {
+    INFO(Level.INFO),
+    WARNING(Level.WARNING),
+    ERROR(Level.SEVERE),
+    CRITICAL(Level.OFF);
+
+    private Level standardLevel;
+
+    AraneaLoggerLevels(Level level) {
+      this.standardLevel = level;
+    }
+
+    static String convertStandardToCustomLabel(Level level) {
+      if (level == Level.WARNING) return "WARNING";
+      else if (level == Level.SEVERE) return "ERROR";
+      else if (level == Level.OFF) return "CRITICAL";
+      return "INFO";
+    }
+  }
+
+  private static class AraneaLoggerFormatter extends Formatter {
+    @Override
+    public String format(LogRecord record) {
+      return "["
+          + (new SimpleDateFormat("dd.MM.yy hh:mm:ss")).format(new Date(record.getMillis()))
+          + "] "
+          + AraneaLoggerLevels.convertStandardToCustomLabel(record.getLevel())
+          + ": "
+          + record.getMessage()
+          + "\n";
+    }
+  }
+
+  private static class AraneaLoggerFilter implements Filter {
+    @Override
+    public boolean isLoggable(LogRecord record) {
+      return record.getLevel().intValue() >= minLevel.intValue();
+    }
+  }
+
+  private void exemplifyUsage(){
+    AraneaLogger logger = AraneaLogger.getInstance();
+    logger.setOutputFile("log.txt");
+    logger.setMinLevel(AraneaLoggerLevels.WARNING);
+    logger.log(AraneaLoggerLevels.INFO, "Information example");
+    AraneaLogger another_logger = AraneaLogger.getInstance();
+    another_logger.log(AraneaLoggerLevels.ERROR, "Error example");
+  }
+}
