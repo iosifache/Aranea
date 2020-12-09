026f5d8b339044935a2f3c8a88e2696313c7162d
i014n <ionutm.bajan@yahoo.com>
2020-12-09 22:31:53 +0200

    Implementează numai clasa Aranea
    Issue: #11
    
    Am eleminat clasele care nu erau în responsabilitatea mea.
    Exista clasa Sieve aici, deoarece am adaugat o metodă specifică.
    
    Signed-off-by: i014n <ionutm.bajan@yahoo.com>

--- aranea/src/com/aranea/PatternFinder.java
+++
@@ -1,6 +0,0 @@
-package com.aranea;
-
-public class PatternFinder {
-
-    public PatternFinder(String rootDir, String pattern) {};
-}
--- aranea/src/com/aranea/SitemapGenerator.java
+++
@@ -1,6 +0,0 @@
-package com.aranea;
-
-public class SitemapGenerator {
-
-    public SitemapGenerator(String downloadDir, String outfile) {};
-}
