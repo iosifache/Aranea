c75237510a6d636c4119936f5ddb01bedb6381bd
apostolescus <apostolescus@outlook.com>
2020-12-11 10:15:18 +0200

    Implementează dimensiune maxima coadă
    Issue: #9
    Oferă posibilitatea setării unei dimensiuni
    maxime a cozii. Programurile se vor opri
    cand se va atinge dimensiunea maximă.
    
    Signed-off-by: apostolescus <apostolescus@outlook.com>

--- aranea/src/com/aranea/URLQueue.java
+++ aranea/src/com/aranea/URLQueue.java
@@ -9,14 +9,22 @@ public class URLQueue {
     private  static int counter;
     private static LinkedHashSet<URL> urlSet;
     private static Iterator<URL> itr;
-
+    private static int maxElements;
     private static URLQueue Singleton;
 
-    private URLQueue() {
+    private URLQueue(int maxElem) {
         urlSet = new LinkedHashSet<URL>();
         itr = null;
+        maxElements = maxElem;
         counter = 0;
-    };
+    }
+
+    public static URLQueue getInstance(int maxElements) {
+        if (urlSet == null) {
+            Singleton = new URLQueue(maxElements);
+        }
+        return Singleton;
+    }
 
     public void PrintUrlQueue(){
         Iterator<URL> iterator;
@@ -27,26 +35,22 @@ public class URLQueue {
         }
         System.out.println("\n");
     }
-    public static URLQueue getInstance(){
-        if (urlSet == null){
-            Singleton = new URLQueue();
-        }
-        return Singleton;
-    }
 
     //if list is empty returns null
     synchronized public URL remove() throws AraneaException {
         int localCounter = 0;
 
-         for(URL retUrl:urlSet){
-             if( localCounter == counter){
-                 System.out.println("Counter is:" + counter);
-                 counter++;
-                 return  retUrl;
-             }
-             localCounter++;
-         }
-         return  null;
+        for (URL retUrl : urlSet) {
+            if (localCounter == counter) {
+                counter++;
+                return retUrl;
+            }
+            if (localCounter == maxElements){
+                return null;
+            }
+            localCounter++;
+        }
+        return null;
     }
 
     public void add(URL new_url){
